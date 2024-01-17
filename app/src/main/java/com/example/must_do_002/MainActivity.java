4ef4848205118d08590d65340375f143b1dbc898
hkhach2008 <hrant.khachatryan.h@gmail.com>
2024-01-17 23:38:32 +0400

    popup menu and starting add task logic

--- app/src/main/java/com/example/must_do_002/MainActivity.java
+++ app/src/main/java/com/example/must_do_002/MainActivity.java
@@ -3,51 +3,20 @@ package com.example.must_do_002;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.PopupWindow;
+import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button_add_task;
-    private PopupWindow popupWindow;
     private Button btn_profile;
+    private Button button_add_task;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button_add_task = findViewById(R.id.button_add_task);
-        button_add_task.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Inflate the popup_menu.xml layout file
-                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupView = inflater.inflate(R.layout.popup_menu, null);
-
-                // Create the PopupWindow object
-                popupWindow = new PopupWindow(
-                        popupView,
-                        android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-                        android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-                        true
-                );
-
-                // Set an elevation value for popup window
-                popupWindow.setElevation(10);
-
-                // Show the popup window
-//                popupWindow.showAsDropDown(button_add_task, 0, 0);
-                int[] location = new int[2];
-                button_add_task.getLocationOnScreen(location);
-                popupWindow.showAtLocation(button_add_task, Gravity.NO_GRAVITY, location[0],location[1] - popupWindow.getHeight());
-            }
-        });
-
         btn_profile = findViewById(R.id.button_profile);
+        button_add_task = findViewById(R.id.button_add_task);
         btn_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,5 +24,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(profile);
             }
         });
+
+        button_add_task.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupAddTask(v);
+            }
+        });
+    }
+
+    private void showPopupAddTask(View v)
+    {
+        PopupMenu popup = new PopupMenu(MainActivity.this, v);
+        popup.getMenuInflater().inflate(R.menu.popup_add_task, popup.getMenu());
+        popup.show();
     }
 }
